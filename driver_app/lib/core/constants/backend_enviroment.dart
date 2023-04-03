0067b50ffa6b4feade872c2ff00fa312fe380571
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-04-03 14:19:47 +0700

    Update backend_enviroment.dart

--- driver_app/lib/core/constants/backend_enviroment.dart
+++ driver_app/lib/core/constants/backend_enviroment.dart
@@ -1,9 +1,10 @@
 class BackendEnviroment {
   static String host = "https://dacnpmbe11.azurewebsites.net/api";
+  static bool isPoor = true;
 
   static checkDevelopmentMode() {
     assert(() {
-      host = "http://192.168.169.168:8001/api";
+      host = "http://192.168.1.14:8001/api";
       return true;
     }());
   }
