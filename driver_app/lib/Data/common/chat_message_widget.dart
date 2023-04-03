99a8f1e532061244cd506aff441fb97cc16ce49c
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-04-03 11:40:30 +0700

    Refactor: refactor chat message widget

--- driver_app/lib/Data/common/chat_message_widget.dart
+++ driver_app/lib/Data/common/chat_message_widget.dart
@@ -12,31 +12,14 @@ class ChatMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
-      color: chatMessageType == ChatMessageType.driver
-          ? Colors.grey[300]
-          : Colors.white,
-      child: Row(
-        children: [
-          Container(
-              margin: const EdgeInsets.only(right: 16),
-              child: chatMessageType == ChatMessageType.driver
-                  ? CircleAvatar(
-                      backgroundColor: const Color.fromRGBO(16, 163, 127, 1),
-                      child: Image.asset(
-                        "assets/icons/face_icon.png",
-                        color: Colors.white,
-                        scale: 1.5,
-                      ),
-                    )
-                  : const CircleAvatar(
-                      child: Icon(Icons.person),
-                    )),
-          Expanded(
+    return chatMessageType == ChatMessageType.driver
+        ? Container(
+            margin: const EdgeInsets.symmetric(vertical: 4),
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+            color: Colors.grey[200],
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Container(
                   padding: const EdgeInsets.all(8),
@@ -52,8 +35,42 @@ class ChatMessageWidget extends StatelessWidget {
               ],
             ),
           )
-        ],
-      ),
-    );
+        : Container(
+            margin: const EdgeInsets.symmetric(vertical: 4),
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+            color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(right: 16),
+                    child: CircleAvatar(
+                      backgroundColor: const Color.fromRGBO(16, 163, 127, 1),
+                      child: Image.asset(
+                        "assets/icons/profile_icon.png",
+                        color: Colors.white,
+                        scale: 1.5,
+                      ),
+                    )),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(text,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                ?.copyWith(color: Colors.black)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
