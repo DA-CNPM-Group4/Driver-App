d1d6e19888686e2dd65f87c26d0c0022ae846def
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-04-03 11:21:24 +0700

    Feat: add chat message widget

---
+++ driver_app/lib/Data/common/chat_message_widget.dart
@@ -0,0 +1,59 @@
+import 'package:driver_app/Data/models/chat_message/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessageWidget extends StatelessWidget {
+  final String text;
+  final ChatMessageType chatMessageType;
+  const ChatMessageWidget({
+    Key? key,
+    required this.text,
+    required this.chatMessageType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+      color: chatMessageType == ChatMessageType.driver
+          ? Colors.white
+          : Colors.grey[300],
+      child: Row(
+        children: [
+          Container(
+              margin: const EdgeInsets.only(right: 16),
+              child: chatMessageType == ChatMessageType.driver
+                  ? CircleAvatar(
+                      backgroundColor: const Color.fromRGBO(16, 163, 127, 1),
+                      child: Image.asset(
+                        "assets/icons/face_icon.png",
+                        color: Colors.white,
+                        scale: 1.5,
+                      ),
+                    )
+                  : const CircleAvatar(
+                      child: Icon(Icons.person),
+                    )),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(text,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          ?.copyWith(color: Colors.black)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
