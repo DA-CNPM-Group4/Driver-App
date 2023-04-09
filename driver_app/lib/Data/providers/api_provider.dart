70f14943a51f318151ce21d62dfe69ab92fb8f05
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-04-09 18:40:43 +0700

    chore

--- driver_app/lib/Data/providers/api_provider.dart
+++ driver_app/lib/Data/providers/api_provider.dart
@@ -52,6 +52,8 @@ class APIHandlerImp implements APIHandlerInterface {
       HttpHeaders.contentTypeHeader: "application/json",
       "device": "app"
     };
+    baseHeader["X-Api-Key"] = "ApplicationKey";
+
     if (useToken) {
       String? token =
           !useRefereshToken ? await getAccessToken() : await getRefreshToken();
